cc1e5b280262fdf3faf5dced6a00f7bf94209a6b
itsmeraju18 <69288937+itsmeraju18@users.noreply.github.com>
2020-08-07 11:24:57 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,33 @@
-﻿using System;
+﻿//C#: Exception Handling: Multiple catch
+using System;
 
-namespace Arrays2
+namespace MultipleCatch
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            int[] array = new int[5] { 1, 2, 3, 4, 5 };
-
-            for (int i = 0; i < array.Length; i++)
-
+            int x = 0;
+            int div = 0;
+            try
             {
-
-                Console.WriteLine(array[i]);
-
+                div = 100 / x;
+                Console.WriteLine("Not executed line");
             }
-
-            Console.WriteLine("Press Enter Key to Exit..");
-
-            Console.ReadLine();
-
+            catch (DivideByZeroException de)
+            {
+                Console.WriteLine("DivideByZeroException");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Finally Block");
+            }
+            Console.WriteLine($"Result is {div}");
+          
         }
-
     }
-
 }
